00dd33d67b049b64bb40543fc46dca4aefe2a817
dave-oh-git <hirakoshinji722@gmail.com>
2019-10-23 17:23:13 +0100

    +++ add phoneIntent with ACTION_DIAL

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -36,6 +36,6 @@ public class InfoActivity extends AppCompatActivity {
     }
 
     public void createPhoneIntent(View view){
-        
+        Intent phoneIntent = new Intent(Intent.ACTION_DIAL);
     }
 }
